aa61afe469f414b9b055510475219dabaf9323a7
rmzturkmen <rmzturkmen72@gmail.com>
2020-10-05 19:37:28 +0200

    milliseconds-converter

--- aws/projects/002-milliseconds-converter/Milliseconds-converter.py
+++ aws/projects/002-milliseconds-converter/Milliseconds-converter.py
@@ -34,5 +34,5 @@ def converter_post():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
