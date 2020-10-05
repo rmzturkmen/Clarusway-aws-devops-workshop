99b78f3a6bc59c211e35e6dfad8c6367301f29f9
rmzturkmen <rmzturkmen72@gmail.com>
2020-10-05 19:36:12 +0200

    milliseconds-converter

--- aws/projects/002-milliseconds-converter/Milliseconds-converter.py
+++ aws/projects/002-milliseconds-converter/Milliseconds-converter.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def converter_get():
     return render_template("index.html", developer_name="Ramazan", not_valid=False)
@@ -12,26 +13,26 @@ def converter_get():
 @app.route("/", methods=["POST"])
 def converter_post():
     num = request.form["number"]
-    
-    if num.isalpha():
-        return render_template("index.html", developer_name="Ramazan", not_valid="not_valid")
-        
+
+    if not num.isdecimal():
+        return render_template("index.html", developer_name="Ramazan", not_valid=True)
+
     elif num.isnumeric():
         numalpha = int(num)
-        
+
         if numalpha < 1:
-            return render_template("index.html", developer_name="Ramazan", not_valid="not_valid")
-            
-        
-        elif numalpha >0:
+            return render_template("index.html", developer_name="Ramazan", not_valid=True)
+
+        elif numalpha > 0:
             hour = numalpha // (1000*60*60)
             minute = (numalpha - hour * (1000*60*60)) // (1000*60)
-            sec = (numalpha - hour * (1000*60*60) - minute * (1000*60)) // (1000)
+            sec = (numalpha - hour * (1000*60*60) -
+                   minute * (1000*60)) // (1000)
             result = (f'{hour} hour/s'*(hour != 0) + f' {minute} minute/s'*(minute != 0) +
-                  f' {sec} second/s' * (sec != 0) or f'just {num} millisecond/s' * (numalpha < 1000))
+                      f' {sec} second/s' * (sec != 0) or f'just {num} millisecond/s' * (numalpha < 1000))
             return render_template("result.html", developer_name="Ramazan", milliseconds=numalpha, not_valid=True, result=result)
-    
+
 
 if __name__ == "__main__":
-      #app.run(debug=True)
-      app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
